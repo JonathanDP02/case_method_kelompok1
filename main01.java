1dda9aee956bcba7eef47840bad77e3e8f0b9d4f
JonathanDP02 <jonatanpangestu23@gmail.com>
2025-04-19 23:13:44 +0700

    main

--- main01.java
+++ main01.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class main01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
+        mahasiswa01[] mhs = {
+            new mahasiswa01("22001", "Ali Rahman", "Informatika"),
+            new mahasiswa01("22002", "Budi Santoso", "Informatika"),
+            new mahasiswa01("22003", "Citra Dewi", "Sistem Informasi Bisnis")
+        };
+
+        mataKuliah01[] mk = {
+            new mataKuliah01("MK001", "Struktur Data", 3),
+            new mataKuliah01("MK002", "Basis Data", 3),
+            new mataKuliah01("MK003", "Desain Web", 3)
+        };
     }
 }
